ff422fc8eb003df264457fbdecd3844fe80bb8a4
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-02-24 17:29:22 +0300

    Change main and BmiService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(double month, double money, double percent) {
-        double a = percent/ 12F * 0.01; // ежемесечный процент
-        double ka = (a*Math.pow((1+a),month))/(Math.pow((1+a),month)-1); //коэффициент аннуитета
+        double a = percent / 12F * 0.01; // ежемесечный процент
+        double ka = (a * Math.pow((1 + a), month)) / (Math.pow((1 + a), month) - 1); //коэффициент аннуитета
         double result = money * ka; // аннуитетный платеж
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int month = 36; // На сколько месяцев кредит
+        int month = 24; // На сколько месяцев кредит
         double money = 1_000_000; // Сумма кредита
         double percent = 9.9; // Процентная ставка
-        double annuityPayment =  service.calculate(month, money, percent);
-        System.out.println("Ежемесечный платеж будет: "+ annuityPayment);
+        double annuityPayment = service.calculate(month, money, percent);
+        System.out.println("Ежемесечный платеж будет: " + annuityPayment);
     }
 }
